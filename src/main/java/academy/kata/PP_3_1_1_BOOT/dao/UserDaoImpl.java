f43bea247e128063caa17ca2f375e007dff21069
KapustinS <sergey.a.kapustin@gmail.com>
2022-07-20 21:45:29 +0300

    add EntityManager

---
+++ src/main/java/academy/kata/PP_3_1_1_BOOT/dao/UserDaoImpl.java
@@ -0,0 +1,2 @@
+package academy.kata.PP_3_1_1_BOOT.dao;public class UserDaoImpl {
+}
--- src/main/java/academy/kata/PP_3_1_1_BOOT/repositories/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package academy.kata.PP_3_1_1_BOOT.repositories;
-
-import academy.kata.PP_3_1_1_BOOT.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepository extends JpaRepository<User, Integer> {
-}
